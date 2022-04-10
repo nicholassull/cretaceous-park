cce81d531238772b83fc3b9c5ec342eef566ddbc
Nick Sullivan <nicholassull@gmail.com>
2022-04-10 11:14:22 -0700

    add DELETE request in AnimalsController

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -76,5 +76,20 @@ namespace CretaceousPark.Controllers
     {
       return _db.Animals.Any(e => e.AnimalId == id);
     }
+
+    // DELETE: api/animals/2
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal ==  null)
+      {
+        return NotFound();
+      }
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
